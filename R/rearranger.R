8f2ae02fceb76985da3397ad7fcec470708f02c2
John Brehm <jjbrehm@uchicago.edu>
2020-07-21 14:03:00 -0500

    Fix

--- R/rearranger.R
+++ R/rearranger.R
@@ -10,7 +10,7 @@
 #' @examples
 rearranger <- function(simobj) {
   pr.t <- tidyr::as_tibble(simobj$Performance)
-  pr.t <- pr.t %>% tidyr::pivot_longer(cols=3:(dim(pr)[2]), names_to="Worker", values_to="Performance") %>%
+  pr.t <- pr.t %>% tidyr::pivot_longer(cols=3:(dim(pr.t)[2]), names_to="Worker", values_to="Performance") %>%
             rename(Replication = V1,
                   Iteration = V2)
 }
