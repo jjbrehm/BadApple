3b8e9da57f6c99f20399c9f7edfc6321fdbdfecf
John Brehm <jjbrehm@uchicago.edu>
2020-09-24 11:01:15 -0500

    stupid error in peek

--- R/peek.R
+++ R/peek.R
@@ -8,7 +8,7 @@
 #'
 #' @examples peek(simobj, 1)
 peek <- function(simobj, when) {
-  what <- as_tibble(rearranger(foo)) %>%
+  what <- as_tibble(rearranger(simobj)) %>%
     filter(Iteration==when) %>%
     mutate(who=ifelse((Worker=="V3"|Worker=="V4"|Worker=="V5"), "Saboteur", "Other"))
   ggplot(what, aes(x=Performance, fill=who)) + geom_histogram()
